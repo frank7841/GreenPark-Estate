c3fdd07e8a7cb11d19504c34542f32571c543a37
frank7841 <franklinewax@gmail.com>
2021-04-28 16:30:02 +0300

    securiity

--- greenpark_estate/settings.py
+++ greenpark_estate/settings.py
@@ -90,24 +90,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'greenpark_estate.wsgi.application'
 
 
-# Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'nibora_properties',
-        'USER': 'postgres',
-        'PASSWORD': 'p@ssw0rd',
-        'HOST': 'localhost'
-    }
-}
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'mail.niboraproperties.co.ke'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'info@niboraproperties.co.ke'
-EMAIL_HOST_PASSWORD = '.Ov}bo&]UMI;'
-EMAIL_USE_TLS = True
-
 
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
